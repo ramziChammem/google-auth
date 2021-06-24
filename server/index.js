42f8e484617aa39b3f5b3c1b2569a0cb533bf57b
ramziChammem <ramzichammem69@gmail.com>
2021-06-24 16:31:03 +0100

    setting mysql database with sequelize orm

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-
-
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/../client/public/"));
+app.use(express.static(__dirname + "/../react-client/public/"));
 app.use(bodyParser.json());
-
 
 app.listen(3000, function () {
   console.log("listening on port 3000!");
